e6e986e11a528ea714b49662d061b564bd2423d6
wali <larts393@gmail.com>
2021-07-15 21:40:44 +0800

    a

---
+++ a.js
@@ -0,0 +1,101 @@
+// console.log("hi 中文也行");
+// console.log('hi 中文也行');
+// console.log('    hi 中文也行');
+// console.log(`hi wali`)
+// console.log('hello world')
+
+// var a = "hi"
+// console.log(a)
+// console.log("a")
+
+// var a
+// console.log(a)
+
+// console.log(a)
+
+// var a = null
+// console.log(a)
+
+// var a = 1
+// var b = 2
+// console.log(a + b)
+
+// var a = 1
+// var b = "2"
+// console.log(a + b)
+
+// var a = "3"
+// var b = "2"
+// console.log(a + b)
+
+// console.log(a)
+// var a = 1
+
+// 變數提昇
+// console.log(a)  undefined
+// var a = 1
+// console.log(a) 1
+
+// let a = 1
+// console.log(a)
+
+// console.log(a)
+// let a = 1
+
+// var a = 1;
+// var a;
+// console.log(a);
+
+// console.log(a)
+// let a = 1
+
+// var age = 22
+// if (age >= 20) {
+//   console.log("投票")
+// } else {
+//   console.log("不投票")
+// }    
+
+// var age = 22
+// var citizen = true
+
+// if (age >= 20) {
+//   if (citizen == true){
+//       console.log('可以投票')
+//     } else {
+//       console.log('不能投票')
+//     }
+
+// age = 16
+// citizen = true
+
+// if (age>=20 && citizen){
+//     console.log("可以投票")
+// } else {
+//     console.log("不可以投票")
+// }
+
+// age = 16
+// citizen = true
+
+// if (age>=20 || citizen){
+//     console.log("可以投票")
+// } else {
+//     console.log("不可以投票")
+// }
+
+
+// var a = 10
+// var b = 2
+// console.log(a && b)
+
+// var a = false
+// var b = 2
+// console.log(a && b)
+
+// var a = true
+// var b = 2
+// console.log(a && b)
+
+
+
