89ea269b69d1ccb62094cff31b860d255e6f4f6a
wali <larts393@gmail.com>
2021-07-15 21:41:31 +0800

    b

---
+++ b.js
@@ -0,0 +1,12 @@
+let items = ["a", 7, "c", "d"]
+// console.log(items[0])
+// console.log(items[1])
+// console.log(items[2])
+// console.log(items[3])
+// console.log(items.length)
+// console.log(items.length - 1)
+// console.log(items[items.length-1])
+
+var lastOne = items.length-1
+console.log(items[lastOne])
+
